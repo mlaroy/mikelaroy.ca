aa7ae1d1d9e50158d6895a5f6357edbfc8358b29
Michael LaRoy <mike.laroy@gmail.com>
2019-02-06 21:13:34 -0800

    purgecss config

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,17 @@ module.exports = {
       },
     },
     'gatsby-plugin-catch-links',
-    'gatsby-plugin-purgecss',
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        whitelist: ['.article-body .gatsby-resp-image-wrapper'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
